2ab9507f423d41e44490eda483f042e8293a45cc
memomu <gmunoz.u8@gmail.com>
2021-10-05 23:59:07 -0500

    Update reporte_proyecto.py

--- webapp/pages/reporte_proyecto.py
+++ webapp/pages/reporte_proyecto.py
@@ -12,13 +12,6 @@ def app():
 
     st.title("Reporte del proyecto")
     st.title("App Web para Monitoreo de Portafolio")
-    st.write("Muñoz, Guillermo")
-    st.write("Ortíz-Monasterio, Pedro")
-    st.write("Trillas, Josune") 
-    st.write("Procesamiento de lenguaje natural")
-    st.write("2do semestre")
-    st.write("Profesor PHD Raul Morales Salcedo")
-    st.write("06 de octubre del 2021")
     
     with open("reporte_proyecto_eq3.pdf", "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
