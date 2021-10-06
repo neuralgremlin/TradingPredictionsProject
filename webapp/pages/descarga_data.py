f669ffcada97492e26cc158f02dd1e119de6e05a
memomu <gmunoz.u8@gmail.com>
2021-10-06 00:22:03 -0500

    Update descarga_data.py

--- webapp/pages/descarga_data.py
+++ webapp/pages/descarga_data.py
@@ -8,13 +8,13 @@ import pandas as pd
 
 def app():
 
-    st.title("Selección de Stocks y descarga de datos")
+    st.title("Selección de Stocks y visualización de datos")
 
     START = "2015-01-01"
     TODAY = date.today().strftime("%Y-%m-%d")
 
     df_stocks = pd.read_csv("stocks.csv")
-    
+
     st.write("Apple Inc. - AAPL")
     st.write("Amazon Inc. - AMZN")
     st.write("NVIDIA Corp. - NVIDIA")
@@ -26,9 +26,9 @@ def app():
     st.write("Adobe Inc. - ADBE")
     st.write("Mastercard Inc. - MA")
     st.write(" ")
-    
+
     stocks = df_stocks["Símbolo"]
-    selected_stock = st.selectbox("Elige una acción para descargar su csv", stocks)
+    selected_stock = st.selectbox("Elige una acción para visualizar un dataframe", stocks)
 
     @st.cache
     def load_data(ticker):
@@ -39,27 +39,26 @@ def app():
     data = load_data(selected_stock)
     data_load_state = st.text("Cargando data... listo!")
 
-    if st.checkbox('Descargar data de la acción ' + selected_stock):
-        data.to_csv(selected_stock+".csv")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.title("Data descargada!")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.write(" ")
-        st.header("Echa un vistazo a la data")
-        st.subheader("Información de " + selected_stock)
-        st.write(data.tail())
+    @st.cache
+    def convert_df(df):
+        return df.to_csv().encode('utf-8')
+    csv = convert_df(data)
 
-        def plot_raw_data():
-            fig = go.Figure()
-            fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-            fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-            fig.layout.update(title_text="Comportamiento de la acción. Desliza la barra de abajo para ver un periodo más corto.", xaxis_rangeslider_visible=True)
-            st.plotly_chart(fig)
-        plot_raw_data()
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=selected_stock+'.csv',
+        mime='text/csv',
+        )
+
+    st.header("Echa un vistazo a la data")
+    st.subheader("Información de " + selected_stock)
+    st.write(data)
+
+    def plot_raw_data():
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+        fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+        fig.layout.update(title_text="Comportamiento de la acción. Desliza la barra de abajo para ver un periodo más corto.", xaxis_rangeslider_visible=True)
+        st.plotly_chart(fig)
+    plot_raw_data()
