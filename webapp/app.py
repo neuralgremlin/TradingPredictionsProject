83a3f4f737d6771f59ccb6b0816212d24e69af33
memomu <gmunoz.u8@gmail.com>
2021-10-06 00:23:15 -0500

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -12,7 +12,7 @@ st.title("Proyecto - Cómputo en la nube")
 
 # Add all your applications (pages) here
 app.add_page("Aplicación",main.app)
-app.add_page("Visualiza la data",descarga_data.app)
+app.add_page("Descarga la data",descarga_data.app)
 app.add_page("Otros datos",otros_datos.app)
 app.add_page("Reporte del proyecto", reporte_proyecto.app)
 
